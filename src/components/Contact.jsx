2fbbca41cd75c364b852a5c2c2381fe227ca7fba
Justin Zhu <justinzhu0913@gmail.com>
2024-09-27 14:16:37 -0400

    updated About.jsx and Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     return (
         <section id="contact">
             <div className="mx-auto max-w-6xl">
-                <p className="my-10 text-center text-3xl lg:text-8xl">Interested In Working Together?</p>
+                <p className="my-10 text-center text-3xl lg:text-6xl">Interested In Working Together?</p>
                 <p className="p-4 text-center text-xl">{CONTACT.text}</p>
                 <p className="my-4 text-center text-2xl font-medium text-lime-300 lg:pt-6 lg:text-5xl">{CONTACT.email}</p>
                 <p className="my-4 text-center text-2xl font-medium text-lime-300 lg:pb-6 lg:text-5xl">{CONTACT.phone}</p>
