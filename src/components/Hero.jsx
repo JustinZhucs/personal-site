653bca16f045e7ca628e0f563746e1658dd3bb3f
Justin Zhu <justinzhu0913@gmail.com>
2024-10-08 14:42:38 -0400

    updated downloadable resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
                     Justin <br /> Zhu
                 </h1>
                 <div className="mt-8">
-                    <a href="/Benson-Langford.pdf"
+                    <a href="public/Resume-Chenshuo-Zhu.pdf"
                         target="_blank"
                         rel="noopener noreferrer"
                         download
