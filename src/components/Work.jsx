8577bd8f52bc69e336a9e650c7467dbc796d8cd6
Justin Zhu <justinzhu0913@gmail.com>
2024-10-08 15:16:44 -0400

    changes for responsive design

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -8,12 +8,12 @@ const Work = () => {
             <div className="mx-10 lg:mx-20">
                 {EXPERIENCES.map((experience, id) => (
                     <div key={id} className="mx-4 mb-20">
-                        <h2 className="font-medium lg:text-2xl">{experience.company}</h2>
+                        <h2 className="font-medium text-xl lg:text-2xl">{experience.company}</h2>
                         <div className="flex justify-between">
-                            <p className="py-4 tracking-wide lg:text-xl">{experience.role}</p>
-                            <p className="py-4 lg:text-xl">{experience.year}</p>
+                            <p className="py-4 tracking-wide text-lg lg:text-xl">{experience.role}</p>
+                            <p className="py-4 text-lg lg:text-xl">{experience.year}</p>
                         </div>
-                        <p className="font-sans text-gray-400 text-lg ">{experience.description}</p>
+                        <p className="font-sans text-gray-400 text:md lg:text-lg ">{experience.description}</p>
                     </div>
                 ))}
             </div>
